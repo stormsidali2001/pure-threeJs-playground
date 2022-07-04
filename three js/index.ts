a82bf480b68060753d4de10b77334f379337c658
stormsidali2001 <assoulsidali@gmail.com>
2022-07-04 16:20:47 +0200

    simple plane geometry

--- three js/index.ts
+++ three js/index.ts
@@ -1,8 +1,8 @@
 import * as THREE from 'three'
 var camera: THREE.PerspectiveCamera | THREE.Camera,
-    geometry,
-    material,
-    mesh: THREE.Object3D<THREE.Event> | THREE.Mesh<THREE.IcosahedronGeometry, THREE.MeshBasicMaterial>,
+    planGeometry,
+    planeMaterial,
+    planeMesh: THREE.Object3D<THREE.Event> | THREE.Mesh<THREE.IcosahedronGeometry, THREE.MeshBasicMaterial>,
     scene: THREE.Object3D<THREE.Event>,
     renderer: THREE.WebGLRenderer
     ;
@@ -11,17 +11,17 @@ function setup(container: HTMLElement ){
   camera = new THREE.PerspectiveCamera( 70, container.getBoundingClientRect().width /  container.getBoundingClientRect().height, 0.1, 1000 );
   camera.position.z = 5;
   scene = new THREE.Scene();
-  geometry = new THREE.BoxGeometry(1,1,1);
-  material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-  mesh = new THREE.Mesh(geometry, material);
-  console.log(mesh)
+  planGeometry = new THREE.PlaneGeometry(5,5,10,10);
+  planeMaterial = new THREE.MeshBasicMaterial({color: 0xff0000,side:THREE.DoubleSide});
+  planeMesh = new THREE.Mesh(planGeometry, planeMaterial);
+  console.log(planeMesh)
   //adding object to the scene------------------------------------------------
-  scene.add( mesh );
+  scene.add( planeMesh );
 
 }
 function animation( time: number ) {
-  mesh.rotation.x = time/2000
-  mesh.rotation.y = 0;
+  planeMesh.rotation.x = time/2000
+  planeMesh.rotation.y = 0;
   renderer.render( scene, camera );
 }
 export function bootstrapThree(containerId:string){
